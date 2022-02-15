3934b246537dd632ae49f94ba3802f25cf06fc5d
dipteekhd <dipteekahde7778@gmail.com>
2022-02-16 00:52:14 +0530

    feat : e-com landing page

---
+++ index.js
@@ -0,0 +1,13 @@
+const menuToggle = document.querySelector(".menu__button");
+const drawer = document.querySelector(".drawer");
+const menu = document.querySelector(".menu");
+
+menuToggle.addEventListener("click", () => {
+  if (menu.style.display || menu.style.display !== "none") {
+    drawer.style.display =
+      !drawer.style.display || drawer.style.display == "none"
+        ? "block"
+        : "none";
+  } else {
+  }
+});
